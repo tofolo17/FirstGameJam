5219de95f61f947cf7579260ec200172d0e6218f
GUSTAVO TOFOLO <gustavo.tofolo@etec.sp.gov.br>
2020-05-31 14:44:26 -0300

    Mapa e física

--- Main.py
+++ Main.py
@@ -1,27 +1,34 @@
-import pygame as pg
-import sys
 import os
+import sys
+
+import pygame as pg
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-# Verificando erros de inicializacao
-check_errors = pg.init()
-if check_errors[1] > 0:
-    print(f"(!) Ops, {check_errors[1]} o Pygame iniciou com algum problema...")
-    sys.exit(-1)
-else:
-    print("(+) O Pygame foi inicializado com sucesso!")
+pg.init()
 
 # Tamanho da tela e título
-info_object = pg.display.Info()
-win_size = [info_object.current_w - 5, info_object.current_h - 40]
-screen = pg.display.set_mode((win_size[0], win_size[1]))
+win_size = [pg.display.Info().current_w - 5, pg.display.Info().current_h - 40]
+screen = pg.display.set_mode(size=win_size)
 pg.display.set_caption('Rocket Wave')
+display = pg.Surface((300, 200))
 clock = pg.time.Clock()
 
-# Colors
-white = (255, 255, 255)
-black = (0, 0, 0)
+# Mapa
+game_map = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '2', '2', '2', '2', '2', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['2', '2', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '2', '2'],
+            ['1', '1', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1']]
+grass_img = pg.image.load('Imagens//grass.png')
+dirt_img = pg.image.load('Imagens//dirt.png')
 
 '''
 def game_intro():
@@ -46,48 +53,95 @@ def game_intro():
 '''
 
 
+def collision_test(rect, tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
+
+def move(rect, movement, tiles):
+    collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
+    rect.x += movement[0]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types['right'] = True
+        elif movement[0] < 0:
+            rect.left = tile.right
+            collision_types['left'] = True
+    rect.y += movement[1]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types['bottom'] = True
+        elif movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types['top'] = True
+    return rect, collision_types
+
+
 # Loop principal
 def game_loop():
 
     # Variáveis do loop
-    game_exit = False
-    moving_right = False
-    moving_left = False
-    player_location = [50, 50]
-    player_y_momentum = 0
-    player_image = pg.image.load('Imagens//background.jpg').convert()
-    player_rect = pg.Rect(player_location[0], player_location[1], player_image.get_width(), player_image.get_height())
-    test_rect = pg.Rect(100, 100, 100, 50)
+    game_exit = moving_left = moving_right = False
+
+    # Variáveis físicas
+    vertical_momentum = 0
+    air_timer = 0
+
+    # Personagem
+    player_image = pg.image.load('Imagens//player.png').convert()
+    player_image.set_colorkey((255, 255, 255))
+    player_rect = pg.Rect(100, 116, 5, 13)
 
     # Enquanto o jogo estiver aberto...
     while not game_exit:
 
         # Atualizando a tela
-        screen.fill(black)
-        screen.blit(player_image, player_location)
+        display.fill((146, 244, 255))
 
-        # Gravidade
-        if player_location[1] > win_size[1] - player_image.get_height():
-            player_y_momentum = -player_y_momentum
-        else:
-            player_y_momentum += 0.2
-        player_location[1] += player_y_momentum
+        # Construção do mapa
+        tile_rect = []
+        y = 0
+        for layer in game_map:
+            x = 0
+            for tile in layer:
+                if tile == '1':
+                    display.blit(dirt_img, (x * 16, y * 16))
+                if tile == '2':
+                    display.blit(grass_img, (x * 16, y * 16))
+                if tile != '0':
+                    tile_rect.append(pg.Rect(x * 16, y * 16, 16, 16))
+                x += 1
+            y += 1
 
-        # Velocidade
+        # Movimento do personagem
+        player_movement = [0, 0]
         if moving_right:
-            player_location[0] += 4
+            player_movement[0] += 2
         if moving_left:
-            player_location[0] -= 4
+            player_movement[0] -= 2
+        player_movement[1] += vertical_momentum
+        vertical_momentum += 0.2
+        if vertical_momentum > 3:
+            vertical_momentum = 3
 
-        # Posições do rect do personagem
-        player_rect.x = player_location[0]
-        player_rect.y = player_location[1]
+        player_rect, collisions = move(player_rect, player_movement, tile_rect)
 
-        # Teste de colisão
-        if player_rect.colliderect(test_rect):
-            pg.draw.rect(screen, (255, 0, 0), test_rect)
+        # Mantém o personagem colidindo com o chão
+        if collisions['bottom'] or collisions['top']:
+            air_timer = 0
+            vertical_momentum = 0
         else:
-            pg.draw.rect(screen, (0, 255, 0), test_rect)
+            air_timer += 1
+
+        # Põe o personagem na tela
+        display.blit(player_image, (player_rect.x, player_rect.y))
 
         # Apurando eventos
         for event in pg.event.get():
@@ -98,6 +152,9 @@ def game_loop():
                     moving_left = True
                 if event.key == pg.K_RIGHT:
                     moving_right = True
+                if event.key == pg.K_UP:
+                    if air_timer < 6:  # Está relacionado ao momento vertical (se você o aumenta, o tempo no ar diminui)
+                        vertical_momentum = -5
             if event.type == pg.KEYUP:
                 if event.key == pg.K_LEFT:
                     moving_left = False
@@ -105,6 +162,7 @@ def game_loop():
                     moving_right = False
 
         # Mostrando para o usuário e FPS
+        screen.blit(pg.transform.scale(display, win_size), (0, 0))
         pg.display.update()
         clock.tick(60)
 
