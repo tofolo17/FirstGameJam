3ac428cc29405c77cf1cf6b4bee7dcd36f9087b2
Victor <wickboldvictor@gmail.com>
2020-05-31 01:27:23 -0300

    atualização

--- Main.py
+++ Main.py
@@ -1,5 +1,15 @@
 import pygame
+import os
 
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
+SCREEN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.display.set_caption('"Trocar nome"')
+while True:
 
-# Ainda não vamos alterar nada!
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.update()
